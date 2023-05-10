884951b800455deac2b9270ff753fd13e3c31a45
Dylan Lester <dylanclester@gmail.com>
2023-05-11 02:24:47 +1000

    add Braid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "braid-design-system/reset";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import apacTheme from "braid-design-system/themes/apac";
+import { BraidProvider } from "braid-design-system";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <BraidProvider theme={apacTheme}>
+      <Component {...pageProps} />
+    </BraidProvider>
+  );
 }
